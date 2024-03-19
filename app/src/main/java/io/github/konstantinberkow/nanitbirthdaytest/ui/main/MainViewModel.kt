cade29d6dc882a1009ee8fbfc417338b949b0e13
Kostiantyn Berkov <yatsok@Kostiantyns-MacBook-Pro.local>
2024-03-19 18:25:56 +0200

    Create ViewModel which controls websocket

--- app/src/main/java/io/github/konstantinberkow/nanitbirthdaytest/ui/main/MainViewModel.kt
+++ app/src/main/java/io/github/konstantinberkow/nanitbirthdaytest/ui/main/MainViewModel.kt
@@ -1,15 +1,114 @@
 package io.github.konstantinberkow.nanitbirthdaytest.ui.main
 
+import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.CreationExtras
 import io.github.konstantinberkow.nanitbirthdaytest.NanitClientApp
 import io.github.konstantinberkow.nanitbirthdaytest.network.WebSocketClient
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.onStart
+
+private const val TAG = "MainViewModel"
 
 class MainViewModel(
     private val webSocketClientFactory: () -> WebSocketClient
 ) : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    // only existing action - connect
+    private val actionsFlow = MutableStateFlow("")
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val stateLiveData: LiveData<State> =
+        actionsFlow
+            .filter { it.isNotBlank() } // prevent default
+            .flatMapLatest { address ->
+                channelFlow {
+                    send(State.connecting(address))
+                    val client = webSocketClientFactory()
+                    client.subscribe(
+                        input = flowOf(WebSocketClient.Command.Connect(address)),
+                        stateUpdated = { socketState ->
+                            Log.d(
+                                TAG,
+                                "Latest socket state: $socketState, thread: ${Thread.currentThread()}"
+                            )
+
+                            val newState = when (socketState) {
+                                is WebSocketClient.Event.State.Closed,
+                                is WebSocketClient.Event.State.Closing ->
+                                    State.closed(address)
+
+                                is WebSocketClient.Event.State.Connected ->
+                                    State.connected(address)
+
+                                is WebSocketClient.Event.State.Failed ->
+                                    State.failed(address, socketState.throwable)
+                            }
+                            send(newState)
+                        }
+                    )
+                }
+            }
+            .onStart {
+                emit(
+                    State()
+                )
+            }
+            .asLiveData(context = viewModelScope.coroutineContext)
+
+    fun setWebSocketAddress(address: String) {
+        Log.d(TAG, "setWebSocketAddress: $address")
+        actionsFlow.tryEmit(address)
+    }
+
+    data class State(
+        val webSocketAddress: String = "",
+        val connecting: Boolean = false,
+        val connected: Boolean = false,
+        val failure: Throwable? = null
+    ) {
+
+        companion object {
+
+            fun connecting(address: String) =
+                State(
+                    webSocketAddress = address,
+                    connecting = true,
+                    connected = false
+                )
+
+            fun connected(address: String) =
+                State(
+                    webSocketAddress = address,
+                    connecting = false,
+                    connected = true
+                )
+
+            fun closed(address: String) =
+                State(
+                    webSocketAddress = address,
+                    connecting = false,
+                    connected = false
+                )
+
+            fun failed(address: String, cause: Throwable) =
+                State(
+                    webSocketAddress = address,
+                    connecting = false,
+                    connected = false,
+                    failure = cause
+                )
+        }
+    }
 
     object Factory : ViewModelProvider.Factory {
 
